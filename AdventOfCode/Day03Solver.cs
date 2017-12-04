3ba66a8c27f99788e9c77cb2934c8b7631f6eacf
Michelangelo Accettura <MAccettura@rightpoint.com>
2017-12-04 16:43:39 -0500

    Day 3 part 1

--- AdventOfCode/Day03Solver.cs
+++ AdventOfCode/Day03Solver.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode
 {
@@ -10,16 +6,87 @@ namespace AdventOfCode
     {
         public int Day => 3;
 
-        public string Title => throw new NotImplementedException();
+        public string Title => "Spiral Memory";
 
         public void SolvePart1()
         {
-            throw new NotImplementedException();
+            Console.WriteLine(Part1(368078));
         }
 
         public void SolvePart2()
         {
             throw new NotImplementedException();
+        }
+
+        private static int Part1(int input)
+        {
+            var squareRoot = 3;
+            var gridLevelCounter = 1;
+            while (squareRoot * squareRoot < input)
+            {
+                squareRoot += 2;
+                gridLevelCounter++;
+            }
+
+            Tuple<int, int> coordinate = GetCoordinate(squareRoot, gridLevelCounter, input);
+            return coordinate != null ? GetManhattanDistanceFromCenter(coordinate.Item1, coordinate.Item2) : 0;
+        }
+
+        private static int Part2(int input)
+        {
+            var squareRoot = 3;
+            var gridLevelCounter = 1;
+            while (squareRoot * squareRoot < input)
+            {
+                squareRoot += 2;
+                gridLevelCounter++;
+            }
+
+            Tuple<int, int> coordinate = GetCoordinate(squareRoot, gridLevelCounter, input);
+            return coordinate != null ? GetManhattanDistanceFromCenter(coordinate.Item1, coordinate.Item2) : 0;
+        }
+
+        private static int GetManhattanDistanceFromCenter(int x, int y)
+        {
+            return Math.Abs(0 - x) + Math.Abs(0 - y);
+        }
+
+        private static Tuple<int, int> GetCoordinate(int squareRoot, int gridLevel, int valueToFind)
+        {
+            int x = gridLevel;
+            int y = gridLevel * -1 + 1;
+            int cornerTurn = gridLevel * 8 / 4;
+
+            int currentNum = squareRoot * squareRoot - gridLevel * 8 + 1;
+            for (var sideIndex = 0; sideIndex < 5; sideIndex++)
+            {
+                for (var s = 0; s < cornerTurn; s++)
+                {
+                    if (currentNum == valueToFind)
+                    {
+                        return new Tuple<int, int>(x, y);
+                    }
+
+                    if (sideIndex == 0)
+                    {
+                        y += 1;
+                    }
+                    else if (sideIndex == 1)
+                    {
+                        x -= 1;
+                    }
+                    else if (sideIndex == 2)
+                    {
+                        y -= 1;
+                    }
+                    else if (sideIndex == 3)
+                    {
+                        x += 1;
+                    }
+                    currentNum++;
+                }
+            }
+            return null;
         }
     }
 }
