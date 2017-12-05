322188d34d226b8bbc1cba0e351f29d838bc98c8
Michelangelo Accettura <MAccettura@rightpoint.com>
2017-12-05 13:06:32 -0500

    Added Day 5

--- AdventOfCode/Day05Solver.cs
+++ AdventOfCode/Day05Solver.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AdventOfCode
 {
@@ -10,16 +7,52 @@ namespace AdventOfCode
     {
         public int Day => 5;
 
-        public string Title => throw new NotImplementedException();
+        public string Title => "A Maze of Twisty Trampolines, All Alike";
 
         public void SolvePart1()
         {
-            throw new NotImplementedException();
+            int[] intArray = Properties.Resources.Day5.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(MazeSolver(intArray, GetPart1Offset));
         }
 
         public void SolvePart2()
         {
-            throw new NotImplementedException();
+            int[] intArray = Properties.Resources.Day5.Split(new[] { Environment.NewLine }, StringSplitOptions.None)
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine(MazeSolver(intArray, GetPart2Offset));
+        }
+
+        private static int MazeSolver(int[] input, Func<int, int> getOffset)
+        {
+            var counter = 0;
+            var currentIndex = 0;
+            try
+            {
+                while (true)
+                {
+                    int value = input[currentIndex];
+                    input[currentIndex] = getOffset(value);
+                    currentIndex += value;
+                    counter++;
+                }
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                return counter;
+            }
+        }
+
+        private static int GetPart1Offset(int value)
+        {
+            return value + 1;
+        }
+
+        private static int GetPart2Offset(int value)
+        {
+            return value >= 3 ? value - 1 : value + 1;
         }
     }
 }
