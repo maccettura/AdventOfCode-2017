92fcf74698987a3bebc0b76ef99e9f0be5311a00
Michelangelo Accettura <MAccettura@rightpoint.com>
2017-12-05 16:11:01 -0500

    Cleaned up Day 1

--- AdventOfCode/Day01Solver.cs
+++ AdventOfCode/Day01Solver.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode
@@ -17,53 +16,40 @@ namespace AdventOfCode
 
         public Day01Solver()
         {
-            _inputArray = ConvertStringToIntArray(Properties.Resources.Day1);
+            _inputArray = Properties.Resources.Day1.Select(x => (int) char.GetNumericValue(x)).ToArray();
         }
 
         public void SolvePart1()
         {
-            Console.WriteLine(Part1(_inputArray));
+            var sum = 0;
+            for (var i = 0; i < _inputArray.Length - 1; i++)
+            {
+                if (_inputArray[i] == _inputArray[i + 1])
+                {
+                    sum += _inputArray[i];
+                }
+            }
+            if (_inputArray[0] == _inputArray[_inputArray.Length - 1])
+            {
+                sum += _inputArray[0];
+            }
+
+            Console.WriteLine(sum);
         }
 
         public void SolvePart2()
         {
-            Console.WriteLine(Part2(_inputArray));
-        }
-
-        private static int[] ConvertStringToIntArray(string input)
-        {
-            return input.Select(x => (int)char.GetNumericValue(x)).ToArray();
-        }
-
-        private static int Part1(IReadOnlyList<int> inputArray)
-        {
             var sum = 0;
-            for (var i = 0; i < inputArray.Count - 1; i++)
-            {
-                if (inputArray[i] == inputArray[i + 1])
-                {
-                    sum += inputArray[i];
-                }
-            }
-            if (inputArray[0] == inputArray[inputArray.Count - 1])
-            {
-                sum += inputArray[0];
-            }
-            return sum;
-        }
-
-        private static int Part2(IReadOnlyList<int> inputArray)
-        {
-            var sum = 0;
-            int halfLength = inputArray.Count / 2;
+            int halfLength = _inputArray.Length / 2;
             for (var i = 0; i < halfLength; i++)
             {
-                if (inputArray[i] == inputArray[i + halfLength])
+                if (_inputArray[i] == _inputArray[i + halfLength])
                 {
-                    sum += inputArray[i] * 2;
+                    sum += _inputArray[i] * 2;
                 }
             }
-            return sum;
+
+            Console.WriteLine(sum);
         }
     }
 }
